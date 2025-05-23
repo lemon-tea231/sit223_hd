5e126544888c0501a4385c2bf10ebbb6f5902b98
lemon-tea231 <seneca061123@gmail.com>
2025-05-24 00:59:53 +1000

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,41 +1,22 @@
 package com.training.example.JacocoExample;
 
-public class PalindromeChecker {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Palindrome {
+	
+	public boolean isPalindrome(String inputString) {
+	    if (inputString.length() == 0) {
+	        return true;
+	    } else {
+	    	String rev = "";
+	    	int length = inputString.length();
+	    	 
+	        for ( int i = length - 1; i >= 0; i-- )
+	           rev = rev + inputString.charAt(i);
+	   
+	        if (inputString.equals(rev))
+	           return true;
+	        else
+	           return false;
+	    }
+	}
 
-        // Input
-        System.out.print("Enter a string to check if it's a palindrome: ");
-        String input = scanner.nextLine();
-
-        // Clean and convert to lowercase
-        String cleanedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // Check for palindrome
-        boolean isPalindrome = isPalindrome(cleanedInput);
-
-        // Output
-        if (isPalindrome) {
-            System.out.println("Yes, it's a palindrome.");
-        } else {
-            System.out.println("No, it's not a palindrome.");
-        }
-
-        scanner.close();
-    }
-
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
 }
